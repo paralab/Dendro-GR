5e438d9cd09eef0b8c8d4656b6d6e610dfef5561
milindasf <milinda@cs.utah.edu>
2023-06-22 13:01:51 -0500

    sc22 gpu experiment scripts copied from sc22-dgr repo

---
+++ BSSN_GR/experiment_scripts/ls6/q1-ws/run_q1_ws_8.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#----------------------------------------------------
+# Sample Slurm job script
+#   for TACC Lonestar6 AMD Milan nodes
+#
+#   *** MPI Job in Normal Queue ***
+# 
+# Last revised: October 22, 2021
+#
+# Notes:
+#
+#   -- Launch this script by executing
+#      "sbatch milan.mpi.slurm" on a Lonestar6 login node.
+#
+#   -- Use ibrun to launch MPI codes on TACC systems.
+#      Do NOT use mpirun or mpiexec.
+#
+#   -- Max recommended MPI ranks per Milan node: 128
+#      (start small, increase gradually).
+#
+#   -- If you're running out of memory, try running
+#      fewer tasks per node to give each task more memory.
+#
+#----------------------------------------------------
+
+#SBATCH -J dgr            # Job name
+#SBATCH -o .dgr.o%j       # Name of stdout output file
+#SBATCH -e .dgr.e%j       # Name of stderr error file
+#SBATCH -p gpu-a100         # Queue (partition) name
+#SBATCH -N 4              # Total # of nodes 
+#SBATCH -n 8              # Total # of mpi tasks
+#SBATCH -t 01:30:00       # Run time (hh:mm:ss)
+#SBATCH --mail-type=all   # Send email at begin and end of job
+##SBATCH -A myproject     # Project/Allocation name (req'd if you have more than 1)
+##SBATCH --mail-user=username@tacc.utexas.edu
+
+# Any other commands must follow all #SBATCH directives...
+module list
+pwd
+date
+
+make bssnWSTestCUDA -j4
+#ibrun -np 1 ./BSSN_GR/bssnSolverCUDA q1_r2.2.par.json 1
+# Launch MPI code... 
+ibrun ./BSSN_GR/bssnWSTestCUDA q1_ws.par.json 1
